f5e13c4dfef5f868ce3ff21a866b6104ad71b668
sandeepsingh2018 <38753664+sandeepsingh2018@users.noreply.github.com>
2020-03-11 18:09:05 +0530

    Add files via upload

---
+++ program to reverse a string.cs
@@ -0,0 +1,38 @@
+﻿/*
+ In this article, we will learn how to reverse a string.
+ 
+This is a frequently asked interview question.
+ 
+Let’s look at the below C# implementation of this algorithm.
+
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class program_to_reverse_a_string
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter string:");
+            string input = Console.ReadLine();
+
+            string result = Reverse(input);
+            Console.WriteLine("reverse string: {0}", result);
+        }
+
+
+        //Reverse a string
+        static string Reverse(string x)
+        {
+            string result = "";
+            for (int i = x.Length - 1; i >= 0; i--)
+                result += x[i];
+            return result;
+        }
+
+    }
+}
