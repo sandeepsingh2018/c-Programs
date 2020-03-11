216d696bae6df784635f912e6c511d09e36bccc2
sandeepsingh2018 <38753664+sandeepsingh2018@users.noreply.github.com>
2020-03-11 17:01:53 +0530

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program3
+{
+    static void Main()
+    {
+        Console.WriteLine("Please provide the sting:");
+        string input = Console.ReadLine();
+
+        string resultString = RemoveDuplicateChars(input);
+
+        Console.WriteLine("Result: {0}", resultString);
+
+    }
+
+    static string RemoveDuplicateChars(string key)
+    {
+        string result = "";
+        string table = "";
+        foreach (var item in key)
+        {
+            if(table.IndexOf(item) == -1)
+            {
+                table = table + item;
+                result = result + item;
+                
+            }
+        }
+        return result;
+    }
+}
+
